5c82ebb6c4efcb73129f422a5fe82750faa949d8
cleybson7 <cleybsonsouza5@gmail.com>
2025-03-24 20:38:32 -0300

    projeto para estudar na faculdade

---
+++ src/main/java/com/example/login_auth_api/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.login_auth_api.repository;
+
+import com.example.login_auth_api.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String login);
+}
+
+
