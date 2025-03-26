f3a82b720e2d72bed4055930d5063fbda717adc0
cleybson7 <cleybsonsouza5@gmail.com>
2025-03-26 14:21:09 -0300

    Finished the security part

--- src/main/java/com/example/login_auth_api/domain/user/User.java
+++ src/main/java/com/example/login_auth_api/domain/user/User.java
@@ -2,16 +2,12 @@ package com.example.login_auth_api.domain.user;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "Users")
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
@@ -23,4 +19,28 @@ public class User {
     private String name;
     private String email;
     private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
